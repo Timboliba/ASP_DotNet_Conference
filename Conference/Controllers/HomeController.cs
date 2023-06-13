20562bcbe4f2d4d3a907635e96b08de2f43bb08f
Hamadi Timboliba <hamaditimboliba54@gmail.com>
2023-06-13 02:00:34 +0100

    secondo

--- Conference/Controllers/HomeController.cs
+++ Conference/Controllers/HomeController.cs
@@ -14,6 +14,7 @@ namespace Conference.Controllers
         private UnitOfWork<Participant> unitParticipant;
         private UnitOfWork<Congre> unitCongre;
 
+
         public HomeController()
         {
             this.unitParticipant = new UnitOfWork<Participant>();
@@ -110,10 +111,17 @@ namespace Conference.Controllers
             ViewBag.ListCongres = model;
             return View();
         }
-
+        
         public ActionResult Logout()
         {
             Session.Clear();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Supprimer(int id, Congre congre)
+        {
+            entity.Congres.Find(congre.Id);
+            entity.Congres.Remove(congre);
             return RedirectToAction("Index");
         }
     }
